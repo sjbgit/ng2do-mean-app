5193909050b11035dc2c7f8a8e181e76cc704f51
sjbgit <sjb_strat@hotmail.com>
2015-06-16 18:04:23 -0500

    base

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sbunke on 6/16/2015.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index.html');
+});
+
+module.exports = router;
